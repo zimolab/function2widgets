09e87c1444feb3afd91485f3745065767c6b99d5
zimolab <zimolab@aliyun.com>
2024-04-01 01:59:49 +0800

    feat(app): add more args to PlainTextEdit

--- function2widgets/widgets/textedit/plaintext.py
+++ function2widgets/widgets/textedit/plaintext.py
@@ -13,6 +13,10 @@ from function2widgets.widgets.base import (
 class PlainTextEditArgs(CommonParameterWidgetArgs):
     parameter_name: str
     default: Optional[str] = ""
+    placeholder: Optional[str] = None
+    readonly: bool = False
+    overwrite_mode: bool = False
+    line_wrap_mode: bool = False
 
 
 class PlainTextEdit(CommonParameterWidget):
@@ -35,6 +39,16 @@ class PlainTextEdit(CommonParameterWidget):
 
     def setup_center_widget(self, center_widget: QWidget):
         self._value_widget = QPlainTextEdit(center_widget)
+
+        if self._args.placeholder:
+            self._value_widget.setPlaceholderText(self._args.placeholder)
+
+        self._value_widget.setReadOnly(self._args.readonly is True)
+        self._value_widget.setOverwriteMode(self._args.overwrite_mode is True)
+        if self._args.line_wrap_mode:
+            self._value_widget.setLineWrapMode(QPlainTextEdit.LineWrapMode.WidgetWidth)
+        else:
+            self._value_widget.setLineWrapMode(QPlainTextEdit.LineWrapMode.NoWrap)
 
         center_widget_layout = QVBoxLayout(center_widget)
         center_widget_layout.setContentsMargins(0, 0, 0, 0)
