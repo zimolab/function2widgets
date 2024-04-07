d8dfe4f1b3f02bc858c2a12514fd922e0b618ac5
zimolab <zimolab@aliyun.com>
2024-04-07 21:11:53 +0800

    feat(app): export Color class

--- function2widgets/widgets/misc/__init__.py
+++ function2widgets/widgets/misc/__init__.py
@@ -1,4 +1,4 @@
 from .datetimeedit import DateTimeEditArgs, DateTimeEdit
 from .timeedit import TimeEditArgs, TimeEdit
 from .dateedit import DateEditArgs, DateEdit
-from .coloredit import ColorEditArgs, ColorEdit
+from .coloredit import ColorEditArgs, ColorEdit, Color
