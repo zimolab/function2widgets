7a10f551281ee1ec67142f0c13a45b43098d9380
zimolab <zimolab@aliyun.com>
2024-04-20 14:12:33 +0800

    refactor(main): add a demo for Qt types
    Test QDateTime QDate QTime QColor types, now these types can be map to
    correct widget types.

---
+++ examples/comprehensive/demo_4.py
@@ -0,0 +1,46 @@
+from PyQt6.QtCore import QDateTime, QDate, QTime
+from PyQt6.QtGui import QColor
+
+from examples.context import ExampleContext
+from function2widgets import FunctionInfoParser, ParameterWidgetFactory
+
+
+def demo3(a: QDateTime, b: QDate, c: QTime, d: QColor):
+    """
+    @widgets
+    [a]
+    display_format = "yyyy-MM-dd HH:mm"
+    calendar_popup = true
+    min_datetime = "2021-11-22 10:10"
+    max_datetime = "2024-12-21 10:10"
+
+    [b]
+    display_format = "yyyy年MM月dd日"
+    calendar_popup = true
+    min_date = "2021年11月22日"
+    max_date = "2024年12月21日"
+
+    [c]
+    display_format = "HH:mm"
+    min_time = "8:30"
+    max_time = "17:30"
+
+    [d]
+    with_alpha=false
+
+    @end
+
+    """
+    pass
+
+
+if __name__ == "__main__":
+    with ExampleContext() as ctx:
+        parser = FunctionInfoParser()
+        func_info = parser.parse(demo3)
+        factory = ParameterWidgetFactory()
+        widgets = factory.create_widgets_for_function(func_info)
+        # start the qt application
+        for widget in widgets.values():
+            ctx.add_widget(widget)
+        ctx.exec()
